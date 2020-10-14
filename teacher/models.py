341687a639600e06e724a7e2e6677dcef5053670
Holly Ansel <holly.ansel@duke.edu>
2020-10-14 14:08:38 -0400

    #9: Issue9 leaderboard data

--- teacher/models.py
+++ teacher/models.py
@@ -9,13 +9,22 @@ class Question(models.Model):
     times_correct = models.IntegerField(default=0)
     tags = ArrayField(models.CharField(max_length=500), size=8, default=list)
     answer = ArrayField(models.IntegerField(), size=8, default=list)
-
+ 
     def get_choices(self):
         return Choice.objects.filter(question=self.id)
 
-
+ 
 class Choice(models.Model):
     id = models.AutoField(primary_key=True)
     body = models.CharField(max_length=500, default="")
     times_chosen = models.IntegerField(default=0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+class Result(models.Model):
+    id = models.AutoField(primary_key=True)
+    level = models.IntegerField(default=0)
+    distance = models.FloatField(default=0.0)
+    # this will later be foreign key to player table
+    player_id = models.IntegerField(default=0)
+    # later have method to get player name
