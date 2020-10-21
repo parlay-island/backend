db899408b91a29404753bec0c7c5df24e5957417
Jake Derry <jacob.derry@duke.edu>
2020-10-20 20:56:54 -0400

    #6 (continued): Added level to question

--- teacher/models.py
+++ teacher/models.py
@@ -2,25 +2,6 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 
-class Question(models.Model):
-    id = models.AutoField(primary_key=True)
-    body = models.CharField(max_length=500, default="")
-    times_answered = models.IntegerField(default=0)
-    times_correct = models.IntegerField(default=0)
-    tags = ArrayField(models.CharField(max_length=500), size=8, default=list)
-    answer = ArrayField(models.IntegerField(), size=8, default=list)
- 
-    def get_choices(self):
-        return Choice.objects.filter(question=self.id)
-
- 
-class Choice(models.Model):
-    id = models.AutoField(primary_key=True)
-    body = models.CharField(max_length=500, default="")
-    times_chosen = models.IntegerField(default=0)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-
-
 class Level(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=500, default="")
@@ -32,7 +13,27 @@ class Result(models.Model):
     distance = models.FloatField(default=0.0)
     # this will later be foreign key to player table
     player_id = models.IntegerField(default=0)
-    
+
     # later have this access the name from the Player Object
     def get_player_name(self):
         return str(self.player_id)
+
+
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    body = models.CharField(max_length=500, default="")
+    times_answered = models.IntegerField(default=0)
+    times_correct = models.IntegerField(default=0)
+    tags = ArrayField(models.CharField(max_length=500), size=8, default=list)
+    answer = ArrayField(models.IntegerField(), size=8, default=list)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, null=True)
+ 
+    def get_choices(self):
+        return Choice.objects.filter(question=self.id)
+
+ 
+class Choice(models.Model):
+    id = models.AutoField(primary_key=True)
+    body = models.CharField(max_length=500, default="")
+    times_chosen = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
