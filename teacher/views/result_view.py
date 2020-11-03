bb5c55c0353670a3c22c584d2c447f3c409f4e3e
Jessica Su <js803@duke.edu>
2020-11-03 14:35:24 -0800

    fixing merge

--- teacher/views/result_view.py
+++ teacher/views/result_view.py
@@ -42,13 +42,10 @@ def post_result(request, player):
             player=player,
             level=Level.objects.get(id=payload[LEVEL]) 
         )
-<<<<<<< HEAD
 
-        update_player_accuracy(player)
-=======
->>>>>>> added distance and level back to response
-        result_serialized = ResultSerializer.serialize(result)
         response_list = update_responses(payload[QUESTIONS], player)
+        update_player_accuracy(player)
+        result_serialized = ResultSerializer.serialize(result)
         response_serialized = [ResponseSerializer.serialize(response) for response in response_list]
         return JsonResponse({'distance': result_serialized[DISTANCE], 'level': result_serialized[LEVEL],
                              'question_results': response_serialized}, safe=False, status=status.HTTP_201_CREATED)
