0a8ac5afaa9a0e973c5e5c792a77e69e43990301
jad111 <jad111@duke.edu>
2020-11-07 15:12:32 -0500

    Added session auth back because of tests but no cookies

--- backend/settings.py
+++ backend/settings.py
@@ -71,6 +71,7 @@ AUTH_USER_MODEL = 'teacher.ParlayUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ]
 }
 
